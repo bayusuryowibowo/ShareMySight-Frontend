e475105f495a082a270655e63a51606be837f313
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-12-24 22:13:39 +0700

    feat: implement animation with framer-motion

---
+++ src/app/lib/placeholder-data.ts
@@ -0,0 +1,9 @@
+import { hand, heart } from "./paths";
+
+export const paths = [hand, heart];
+export const colors = [
+  "#FFDAB9",
+  "#ff0055",
+];
+export const rotations = [[20, -20, 20, -20, 20, -20, 0, 0, 0, 0], [0]];
+export const translationsX = [[5, -5, 5, -5, 5, -5, 0, 0, 0, 0], [0]];
---
+++ src/app/lib/stores/reducers/visibility.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { VisibilityState } from "../definitions";
+
+const initialState: VisibilityState = {
+  isVisible: true,
+};
+
+const visibilitySlice = createSlice({
+  name: "visibility",
+  initialState,
+  reducers: {
+    toggleVisibility: (state) => {
+      state.isVisible = !state.isVisible;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { toggleVisibility } = visibilitySlice.actions;
+
+export default visibilitySlice.reducer;

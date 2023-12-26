5232ef8b63fad8fcbfc433d9cef1b223e0f0c212
Bryan Matthew <bryanmatthew206@gmail.com>
2023-12-26 14:58:54 +0700

    refactor: fix project structure, moving components and lib folder out of app folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,28 @@
-"use client"
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { Provider } from 'react-redux'
-import { useRef } from 'react'
-import { AppStore, makeStore } from './lib/stores'
+"use client";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Provider } from "react-redux";
+import { useRef } from "react";
+import { AppStore, makeStore } from "../lib/stores";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>()
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore()
-  }
+    const storeRef = useRef<AppStore>();
+    if (!storeRef.current) {
+        // Create the store instance the first time this renders
+        storeRef.current = makeStore();
+    }
 
-  return (
-    <html lang="en">
-      <Provider store={storeRef.current}>
-        <body className={inter.className}>{children}</body>
-      </Provider>
-
-    </html>
-  )
+    return (
+        <html lang="en">
+            <Provider store={storeRef.current}>
+                <body className={inter.className}>{children}</body>
+            </Provider>
+        </html>
+    );
 }
--- src/app/ui/hand-love.tsx
+++ src/app/ui/hand-love.tsx
@@ -2,54 +2,58 @@ import * as React from "react";
 import { useState } from "react";
 import { motion, useMotionValue, useTransform, animate } from "framer-motion";
 import { getIndex, useFlubber } from "../use-flubber";
-import { useAppStore } from "../lib/hooks";
-import { toggleVisibility } from "../lib/stores/reducers/visibility";
-import { colors, paths, rotations, translationsX } from "../lib/placeholder-data";
+import { useAppStore } from "../../lib/hooks";
+import { toggleVisibility } from "../../lib/stores/reducers/visibility";
+import {
+    colors,
+    paths,
+    rotations,
+    translationsX,
+} from "../../lib/placeholder-data";
 
 export default function HandLove() {
-  const [pathIndex, setPathIndex] = useState(0);
-  const progress = useMotionValue(pathIndex);
-  const fill = useTransform(progress, paths.map(getIndex), colors);
-  const path = useFlubber(progress, paths);
+    const [pathIndex, setPathIndex] = useState(0);
+    const progress = useMotionValue(pathIndex);
+    const fill = useTransform(progress, paths.map(getIndex), colors);
+    const path = useFlubber(progress, paths);
 
-  const store = useAppStore();
+    const store = useAppStore();
 
-  React.useEffect(() => {
-    const animation = animate(progress, pathIndex, {
-      duration: 0.8,
-      ease: "easeInOut",
-      onComplete: () => {
-        if (pathIndex === paths.length - 1) {
-          console.log("toggleVisibility >>>>")
-          setTimeout(() => {
-            store.dispatch(toggleVisibility())
-          }, 1000);
-        } else {
-          setPathIndex(pathIndex + 1);
-        }
-      }
-    });
-
-    return () => animation.stop();
-  }, [pathIndex]);
-
-  return (
-    <svg className=" absolute overflow-visible" width="400" height="400">
-      <g transform="translate(10 10) scale(17 17)">
-        <motion.path
-          fill={fill}
-          d={path}
-          animate={{
-            rotate: rotations[pathIndex],
-            translateX: translationsX[pathIndex],
-
-          }}
-          transition={{
-            duration: 1.25,
+    React.useEffect(() => {
+        const animation = animate(progress, pathIndex, {
+            duration: 0.8,
             ease: "easeInOut",
-          }}
-        />
-      </g>
-    </svg>
-  );
+            onComplete: () => {
+                if (pathIndex === paths.length - 1) {
+                    console.log("toggleVisibility >>>>");
+                    setTimeout(() => {
+                        store.dispatch(toggleVisibility());
+                    }, 1000);
+                } else {
+                    setPathIndex(pathIndex + 1);
+                }
+            },
+        });
+
+        return () => animation.stop();
+    }, [pathIndex]);
+
+    return (
+        <svg className=" absolute overflow-visible" width="400" height="400">
+            <g transform="translate(10 10) scale(17 17)">
+                <motion.path
+                    fill={fill}
+                    d={path}
+                    animate={{
+                        rotate: rotations[pathIndex],
+                        translateX: translationsX[pathIndex],
+                    }}
+                    transition={{
+                        duration: 1.25,
+                        ease: "easeInOut",
+                    }}
+                />
+            </g>
+        </svg>
+    );
 }

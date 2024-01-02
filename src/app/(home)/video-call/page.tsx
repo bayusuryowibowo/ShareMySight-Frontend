2fc274a6b4092f4f9fe1be3890537e458d690e3e
bayusuryowibowo <bayusuryowibowo@gmail.com>
2024-01-02 14:16:01 +0700

    feat: implement socket-io to video call

--- src/app/(home)/video-call/page.tsx
+++ src/app/(home)/video-call/page.tsx
@@ -1,10 +1,21 @@
-import VideoCall from "@/components/videoCall"
+import Notifications from "@/components/notifications";
+import Options from "@/components/options";
+import VideoPlayer from "@/components/videoPlayer";
+import { Box, Container, Heading } from "@chakra-ui/react";
 
 const VideoCallPage = () => {
   return (
-    <div>
-      <VideoCall />
-    </div>
+    <Box>
+      <Container maxW="1200px" mt="8">
+        <Heading as="h2" size="2xl">
+          {" "}
+          Video Chat App{" "}
+        </Heading>
+        <VideoPlayer />
+        <Options />
+        <Notifications />
+      </Container>
+    </Box>
   )
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { CookiesProvider } from "next-client-cookies/server";
 import { ToastContainer } from "react-toastify";
+import { ChakraProvider } from "@chakra-ui/react";
+import { SocketProvider } from "@/context/socketContext";
 import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,10 +18,14 @@ export default function RootLayout({
         <html lang="en">
             <CookiesProvider>
                 <AuthProvider>
-                    <body className={inter.className}>
-                        {children}
-                        <ToastContainer />
-                    </body>
+                    <SocketProvider>
+                        <ChakraProvider>
+                            <body className={inter.className}>
+                                {children}
+                                <ToastContainer />
+                            </body>
+                        </ChakraProvider>
+                    </SocketProvider>
                 </AuthProvider>
             </CookiesProvider>
         </html>
